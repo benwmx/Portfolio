c36b3b28ff792fdb4099e68c5b7fd8e239d74b7f
medaminedev66 <md.amine.smahi@gmail.com>
2021-07-26 18:01:33 +0100

    Create closeMenu function, make the logo invisible in the menu

--- menu.js
+++ menu.js
@@ -6,9 +6,29 @@ function openMenu(){
     }
     menu.classList.add('mobile-menu');
     document.querySelector('nav > img').classList.toggle('visible');
-    document.querySelector('nav > p').classList.toggle('visible');
+    document.querySelector('nav > div').classList.toggle('visible');
 
 }
 
 let menuBtn = document.querySelector('.menu-icon');
 menuBtn.addEventListener('click',openMenu);
+
+let closeBtn= document.querySelector(".closing-btn");
+closeBtn.addEventListener('click',closeMenu);
+
+function closeMenu(){
+    let menu = document.querySelector('nav>ul');
+    let menuItem = Array.from(document.querySelectorAll('nav > ul li'));
+    for(let item = 0 ; item < menuItem.length ;  item ++){
+        menuItem[item].classList.remove('menu-item');
+    }
+    menu.classList.remove('mobile-menu');
+    document.querySelector('nav > img').classList.toggle('visible');
+    document.querySelector('nav > div').classList.toggle('visible');
+    
+}
+
+let linkClick = Array.from(document.querySelectorAll('nav>ul li'));
+for(let item = 0 ; item < linkClick.length ;  item ++){
+        linkClick[item].addEventListener('click',closeMenu);
+    }
