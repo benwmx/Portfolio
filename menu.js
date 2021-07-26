9f38fca48af1df1c15956b94b35fbde310319e2b
rachid <rachidboudaoudi95@gmail.com>
2021-07-26 16:48:58 +0100

    Edit navbar & openMenu function

--- menu.js
+++ menu.js
@@ -1,8 +1,14 @@
 function openMenu(){
-    console.log('clicked')
-    let menu = document.querySelector('nav>ul')
-    menu.classList.add('mobile-menu')
+    let menu = document.querySelector('nav>ul');
+    let menuItem = Array.from(document.querySelectorAll('nav > ul li'));
+    for(let item = 0 ; item < menuItem.length ;  item ++){
+        menuItem[item].classList.add('menu-item');
+    }
+    menu.classList.add('mobile-menu');
+    document.querySelector('nav > img').classList.toggle('visible');
+    document.querySelector('nav > p').classList.toggle('visible');
+
 }
 
-let menuBtn = document.querySelector('.menu-icon')
-menuBtn.addEventListener('click',openMenu)
+let menuBtn = document.querySelector('.menu-icon');
+menuBtn.addEventListener('click',openMenu);
