cacec2134661b0ba893aaea2943d402eec8a4be6
rachid <rachidboudaoudi95@gmail.com>
2021-07-27 10:07:24 +0100

    Fix linter errors in css & js files

--- menu.js
+++ menu.js
@@ -1,34 +1,32 @@
-function openMenu(){
-    let menu = document.querySelector('nav>ul');
-    let menuItem = Array.from(document.querySelectorAll('nav > ul li'));
-    for(let item = 0 ; item < menuItem.length ;  item ++){
-        menuItem[item].classList.add('menu-item');
-    }
-    menu.classList.add('mobile-menu');
-    document.querySelector('nav > img').classList.toggle('visible');
-    document.querySelector('nav > div').classList.toggle('visible');
-
+function openMenu() {
+  const menu = document.querySelector('nav>ul');
+  const menuItem = Array.from(document.querySelectorAll('nav > ul li'));
+  for (let item = 0; item < menuItem.length; item += 1) {
+    menuItem[item].classList.add('menu-item');
+  }
+  menu.classList.add('mobile-menu');
+  document.querySelector('nav > img').classList.toggle('visible');
+  document.querySelector('nav > div').classList.toggle('visible');
 }
 
-let menuBtn = document.querySelector('.menu-icon');
-menuBtn.addEventListener('click',openMenu);
-
-let closeBtn= document.querySelector(".closing-btn");
-closeBtn.addEventListener('click',closeMenu);
-
-function closeMenu(){
-    let menu = document.querySelector('nav>ul');
-    let menuItem = Array.from(document.querySelectorAll('nav > ul li'));
-    for(let item = 0 ; item < menuItem.length ;  item ++){
-        menuItem[item].classList.remove('menu-item');
-    }
-    menu.classList.remove('mobile-menu');
-    document.querySelector('nav > img').classList.toggle('visible');
-    document.querySelector('nav > div').classList.toggle('visible');
-    
+function closeMenu() {
+  const menu = document.querySelector('nav>ul');
+  const menuItem = Array.from(document.querySelectorAll('nav > ul li'));
+  for (let item = 0; item < menuItem.length; item += 1) {
+    menuItem[item].classList.remove('menu-item');
+  }
+  menu.classList.remove('mobile-menu');
+  document.querySelector('nav > img').classList.toggle('visible');
+  document.querySelector('nav > div').classList.toggle('visible');
 }
 
-let linkClick = Array.from(document.querySelectorAll('nav>ul li'));
-for(let item = 0 ; item < linkClick.length ;  item ++){
-        linkClick[item].addEventListener('click',closeMenu);
-    }
+const menuBtn = document.querySelector('.menu-icon');
+menuBtn.addEventListener('click', openMenu);
+
+const closeBtn = document.querySelector('.closing-btn');
+closeBtn.addEventListener('click', closeMenu);
+
+const linkClick = Array.from(document.querySelectorAll('nav>ul li'));
+for (let item = 0; item < linkClick.length; item += 1) {
+  linkClick[item].addEventListener('click', closeMenu);
+}
