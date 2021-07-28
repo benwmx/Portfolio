bd07e3545130ac657ca632439fa7f99c9a8b1f0e
rachid <rachidboudaoudi95@gmail.com>
2021-07-28 15:33:54 +0100

    Add classes to the new elements of pop up container and style them

--- popup.js
+++ popup.js
@@ -1,51 +1,51 @@
 const projects = [
-    {
+  {
     title: 'My recent works',
-    technologies: ['Html','Bootsrap','Ruby'],
+    technologies: ['Html', 'Bootsrap', 'Ruby'],
     image: 'img/Placeholder.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
     liveLink: '#',
     sourceLink: '#',
-},
-{
+  },
+  {
     title: 'Profesional Art Printing Data',
-    technologies: ['Html','Css','Bootsrap'],
+    technologies: ['Html', 'Css', 'Bootsrap'],
     image: 'img/Placeholder.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
     liveLink: '#',
-    sourceLink: '#'
-},
-{
+    sourceLink: '#',
+  },
+  {
     title: 'Profesional Art Printing Data',
-    technologies: ['Html','Css','Bootsrap'],
+    technologies: ['Html', 'Css', 'Bootsrap'],
     image: 'img/Placeholder.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
     liveLink: '#',
-    sourceLink: '#'
-},
-{
+    sourceLink: '#',
+  },
+  {
     title: 'Profesional Art Printing Data',
-    technologies: ['Html','Css','Bootsrap'],
+    technologies: ['Html', 'Css', 'Bootsrap'],
     image: 'img/Placeholder.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
     liveLink: '#',
-    sourceLink: '#'
-},
-{
+    sourceLink: '#',
+  },
+  {
     title: 'Profesional Art Printing Data',
-    technologies: ['Html','Css','Bootsrap'],
+    technologies: ['Html', 'Css', 'Bootsrap'],
     image: 'img/Placeholder.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
     liveLink: '#',
-    sourceLink: '#'
-}
+    sourceLink: '#',
+  },
 ];
 const container = document.createElement('div');
 const title = document.createElement('h2');
 const infoDiv = document.createElement('div');
 const descBtn = document.createElement('div');
 const description = document.createElement('p');
-const img = document.createElement('img');
+const image = document.createElement('img');
 const technologies = document.createElement('ul');
 const btnOne = document.createElement('a');
 const btnTwo = document.createElement('a');
@@ -65,54 +65,63 @@ container.appendChild(btnClose);
 container.appendChild(technologies);
 container.appendChild(infoDiv);
 
-function pop(index){
+container.className = 'popContainer';
+title.className = 'popTitle';
+description.className = 'popDescr';
+image.className = 'popImage';
+technologies.className = 'popTec';
+btnOne.className = 'popBtn';
+btnTwo.className = 'popBtn';
+
+function pop(index) {
   let techHtml = '';
   document.body.appendChild(container);
   title.innerText = projects[index].title;
-  for(let i = 0 ; projects[index].technologies.length ; i+=1) {
-     techHtml += '<li>'+projects[index].technologies[0]+'</li>';
+  for (let i = 0; projects[index].technologies.length; i += 1) {
+    techHtml += '<li>';
+    techHtml += projects[index].technologies[0];
+    techHtml += '</li>';
   }
   technologies.innerHTML = techHtml;
   img.src = projects[index].image;
-  description.innerText = projects[index].description; 
+  description.innerText = projects[index].description;
   btnOne.href = projects[index].liveLink;
   btnTwo.href = projects[index].sourceLink;
 }
 
-
 // add event listeners
-document.querySelector('.one > button').addEventListener('click',() => {
-    pop(0);
+document.querySelector('.one > button').addEventListener('click', () => {
+  pop(0);
 });
-document.querySelector('.two').addEventListener('click',() => {
-    pop(1);
+document.querySelector('.two').addEventListener('click', () => {
+  pop(1);
 });
-document.querySelector('.two > button').addEventListener('click',() => {
-    pop(1);
+document.querySelector('.two > button').addEventListener('click', () => {
+  pop(1);
 });
-document.querySelector('.three > button').addEventListener('click',() => {
-    pop(2);
+document.querySelector('.three > button').addEventListener('click', () => {
+  pop(2);
 });
-document.querySelector('.three').addEventListener('click',() => {
-    pop(2);
+document.querySelector('.three').addEventListener('click', () => {
+  pop(2);
 });
-document.querySelector('.four > button').addEventListener('click',() => {
-    pop(3);
+document.querySelector('.four > button').addEventListener('click', () => {
+  pop(3);
 });
-document.querySelector('.four').addEventListener('click',() => {
-    pop(3);
+document.querySelector('.four').addEventListener('click', () => {
+  pop(3);
 });
-document.querySelector('.five > button').addEventListener('click',() => {
-    pop(4);
+document.querySelector('.five > button').addEventListener('click', () => {
+  pop(4);
 });
-document.querySelector('.five').addEventListener('click',() => {
-    pop(4);
+document.querySelector('.five').addEventListener('click', () => {
+  pop(4);
 });
-document.querySelector('.six > button').addEventListener('click',() => {
-    pop(5);
+document.querySelector('.six > button').addEventListener('click', () => {
+  pop(5);
 });
-document.querySelector('.six').addEventListener('click',() => {
-    pop(5);
+document.querySelector('.six').addEventListener('click', () => {
+  pop(5);
 });
 
-// closing event
+
