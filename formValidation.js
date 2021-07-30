1e3c299317e5fc1b60586c4379eb403fc8e6fac0
rachid <rachidboudaoudi95@gmail.com>
2021-07-30 17:29:29 +0100

    Change the js function & and styling selector from id to class

--- formValidation.js
+++ formValidation.js
@@ -1,9 +1,14 @@
 const email = document.getElementById('email');
 const form = document.querySelector('form');
-const errorMsg = document.getElementById('error-msg');
+const errorMsg = document.querySelector('form span');
 
+const lowerCaseValidation = (input) => {
+  const lowerInput = input.toLowerCase().trim();
+  if (lowerInput !== input.trim()) return true;
+  return false;
+};
 form.addEventListener('submit', (event) => {
-  if (email.value.trim().toLowerCase() !== email.value.trim()) {
+  if (lowerCaseValidation(email.value)) {
     event.preventDefault();
     errorMsg.className = 'error';
     errorMsg.innerText = 'Please enter email in lowercase format!';
